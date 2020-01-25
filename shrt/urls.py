d38ac21addaf4f05c4696fc36abe050a27ab6716
Hares5323 <58802503+Hares5323@users.noreply.github.com>
2020-01-25 12:55:42 +0530

    Update urls.py

--- shrt/urls.py
+++ shrt/urls.py
@@ -1,4 +1,5 @@
 """shrt URL Configuration
+#Cimmit this code
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
